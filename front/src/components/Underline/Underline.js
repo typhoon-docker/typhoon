0c3947c4343a31de058c7220c53347b4763bf5a6
Ayc0 <ayc0.benj@gmail.com>
2019-01-07 22:50:09 +0100

    Underline

---
+++ front/src/components/Underline/Underline.js
@@ -0,0 +1,59 @@
+import React, { useRef, useEffect } from "react";
+
+import { underline, bold } from "./Underline.css";
+
+const validStyle = [
+  "none",
+  "hidden",
+  "dotted",
+  "dashed",
+  "solid",
+  "double",
+  "groove",
+  "ridge",
+  "inset",
+  "outset",
+  "initial",
+  "inherit"
+];
+
+const Underline = ({
+  as = "span",
+  color = "text",
+  className: cn,
+  variant = "solid",
+  bold: isBold = false,
+  style: s = {},
+  ...props
+}) => {
+  const el = useRef(null);
+
+  useEffect(() => {
+    el.current.style.setProperty("--color", `var(--${color})`);
+  });
+
+  let className = [underline];
+
+  const style = {
+    ...s,
+    borderBottomStyle: variant && validStyle.includes(variant) ? variant : "solid"
+  };
+
+  if (cn) {
+    className.push(cn);
+  }
+  if (isBold) {
+    className.push(bold);
+  }
+
+  className = className.join(" ");
+
+  return React.createElement(as, {
+    ...props,
+    ref: el,
+    className,
+    style
+  });
+};
+
+export default Underline;
