3d23a413fa594fdd582c5d5f3e2ffe721ee630c0
Ayc0 <ayc0.benj@gmail.com>
2019-01-07 15:37:07 +0100

    [Button]: color

--- front/src/components/Button/Button.js
+++ front/src/components/Button/Button.js
@@ -2,9 +2,12 @@ import React from "react";
 
 import { button } from "./Button.css";
 
-const Button = ({ className: cn, ...props }) => {
+const Button = ({ color, className: cn, style: s, ...props }) => {
+  const style = s ? { ...s } : {};
+  style.backgroundColor = `var(--${color || "text"})`;
+
   const className = cn ? [cn, button] : button;
-  return <button {...props} className={className} />;
+  return <button {...props} className={className} style={style} />;
 };
 
 export default Button;
