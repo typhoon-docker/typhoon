5c725bb8a59fef1761ba62b86e11a86e9f22ad18
Ayc0 <ayc0.benj@gmail.com>
2019-01-14 21:24:39 +0100

    [connect]: handle tokens

--- front/src/utils/connect.js
+++ front/src/utils/connect.js
@@ -1,1 +1,52 @@
-export const isConnected = () => true;
+import { decode, sign } from "jsonwebtoken";
+import { shouldMock } from "./env";
+
+const storage = sessionStorage;
+
+const TOKEN_KEY = "token-hjqbgk-oiqjwe-1-4.0";
+
+export const saveToken = token => storage.setItem(TOKEN_KEY, token);
+
+export const removeToken = () => storage.removeItem(TOKEN_KEY);
+
+export const getRawToken = () => storage.getItem(TOKEN_KEY);
+
+export const getToken = () => decode(getRawToken());
+
+export const hasToken = () => {
+  try {
+    const token = getToken();
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+export const isConnected = () => {
+  try {
+    const token = getToken();
+    if (token.exp < Date.now() / 1000 || !token.exp) {
+      throw "";
+    }
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+if (shouldMock) {
+  import("./mock/user.json").then(mockUser => {
+    const iat = parseInt(Date.now() / 1000, 10) - 60;
+    const exp = iat + 3600;
+    saveToken(
+      sign(
+        {
+          user: mockUser,
+          iat,
+          exp
+        },
+        "secret"
+      )
+    );
+  });
+}
