edbdeac58cb9c9359f46218e44597cd38809c2e4
Ayc0 <ayc0.benj@gmail.com>
2019-01-14 21:24:25 +0100

    [env]: function that computes every envs

---
+++ front/src/utils/env.js
@@ -0,0 +1,7 @@
+const MOCK = process.env.MOCK ? process.env.MOCK.toLowerCase() : "";
+const NODE_ENV = process.env.NODE_ENV ? process.env.NODE_ENV.toLowerCase() : "";
+
+export const shouldMock = MOCK === "true";
+export const isProd = NODE_ENV === "production" || NODE_ENV === "prod";
+export const isDev = NODE_ENV === "development" || NODE_ENV === "dev" || NODE_ENV === "";
+export const isTest = NODE_ENV === "test";
