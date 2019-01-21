eb0fafafa90a4dd9bf8593c80a9f1032b6b2459c
Ayc0 <ayc0.benj@gmail.com>
2019-01-21 20:29:07 +0100

    [front]: simple design for existing projects

--- front/src/components/Project/Project.js
+++ front/src/components/Project/Project.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
-const Project = ({ project: { id, name, repository_url, template_id }, onSelect, selected }) => (
-  <details key={id} open={selected}>
-    <summary onClick={onSelect}>{name}</summary>
-    <p>
-      Projet en {template_id}, accessible via <a href={repository_url}>{repository_url}</a>
-    </p>
-  </details>
-);
+import { details, summary, content } from "./Project.css";
+
+const Project = ({ project, onSelect, selected }) => {
+  const { id, name, repository_url, template_id } = project;
+  const el = useRef(null);
+
+  useEffect(() => {
+    el.current.style.setProperty("--border-color", `rgb(var(--${template_id}))`);
+  });
+
+  return (
+    <details key={id} open={selected} className={details}>
+      <summary onClick={onSelect} className={summary}>
+        {name}
+      </summary>
+      <div className={content} ref={el}>
+        Projet en {template_id}, accessible via{" "}
+        <a href={repository_url} target="_blank" rel="noopener noreferrer">
+          {repository_url}
+        </a>
+      </div>
+    </details>
+  );
+};
 
 export default Project;
