b6c9188a475cc56702e4d970276f1bd54bdde29a
Ayc0 <ayc0.benj@gmail.com>
2019-01-21 16:40:27 +0100

    [front]: import mocks at startup

--- front/src/index.js
+++ front/src/index.js
@@ -1,5 +1,13 @@
 import ReactDOM from "react-dom";
+import { shouldMock } from "/utils/env";
+import { importMocks } from "/utils/axios";
 
 import App from "./App";
 
-ReactDOM.render(App, document.getElementById("app"));
+if (shouldMock) {
+  importMocks().then(() => {
+    ReactDOM.render(App, document.getElementById("app"));
+  });
+} else {
+  ReactDOM.render(App, document.getElementById("app"));
+}
