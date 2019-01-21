e1e580ae70fc11c488a105e07941c1004307b1db
Ayc0 <ayc0.benj@gmail.com>
2019-01-21 20:04:59 +0100

    [front]: add Box

---
+++ front/src/components/Box/Box.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { box } from "./Box.css";
+
+const Box = ({ className: cn, ...props }) => {
+  const className = [box];
+  if (cn) {
+    className.push(cn);
+  }
+  return React.createElement("div", {
+    ...props,
+    className: className.join(" ")
+  });
+};
+
+export default Box;
