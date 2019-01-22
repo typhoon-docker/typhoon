a1c354b00cdc19054d2067e4f8bd983e3b33100a
Ayc0 <ayc0.benj@gmail.com>
2019-01-22 22:55:40 +0100

    [front]: button new project

--- front/src/views/Home/Home.js
+++ front/src/views/Home/Home.js
@@ -2,29 +2,42 @@ import React, { useState, useEffect } from "react";
 
 import { getProjects } from "/utils/axios";
 
+import Box from "/components/Box/";
 import Project from "/components/Project/";
+
+import { title, button } from "./Home.css";
 
 const Home = () => {
   const [projects, setProjects] = useState([]);
-  const [selectedProjectID, selectProjectID] = useState(-1);
+  const [selectedProjects, selectProject] = useState({});
 
   useEffect(() => {
     getProjects().then(({ data }) => setProjects(data));
   }, []);
 
-  const selectProject = id => event => {
+  const onSelect = id => event => {
     event.preventDefault();
-    selectProjectID(id !== selectedProjectID ? id : -1);
+    selectProject({ ...selectedProjects, [id]: !selectedProjects[id] });
   };
 
-  return projects.map(project => (
-    <Project
-      key={project.id}
-      project={project}
-      onSelect={selectProject(project.id)}
-      selected={project.id === selectedProjectID}
-    />
-  ));
+  return (
+    <Box>
+      <h1 className={title}>
+        Mes projets
+        <button className={button} type="button">
+          Nouveau projet
+        </button>
+      </h1>
+      {projects.map(project => (
+        <Project
+          key={project.id}
+          project={project}
+          onSelect={onSelect(project.id)}
+          selected={selectedProjects[project.id]}
+        />
+      ))}
+    </Box>
+  );
 };
 
 export default Home;
