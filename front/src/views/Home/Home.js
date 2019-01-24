325272f969484313cce77d4c98359d330a1cbbed
Ayc0 <ayc0.benj@gmail.com>
2019-01-24 16:12:29 +0100

    [front]: change steps transition

--- front/src/views/Home/Home.js
+++ front/src/views/Home/Home.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { getProjects } from '/utils/axios';
 
 import Box from '/components/Box/';
+import Steps from '/components/Steps/';
 import Project from '/components/Project/';
 import EmptyProject from '/components/EmptyProject/';
 
@@ -11,6 +12,7 @@ import { title, button } from './Home.css';
 const Home = () => {
   const [projects, setProjects] = useState(null);
   const [selectedProjects, selectProject] = useState({});
+  const [step, setStep] = useState(0);
 
   useEffect(() => {
     getProjects().then(({ data }) => setProjects(data));
@@ -22,27 +24,29 @@ const Home = () => {
   };
 
   return (
-    <Box>
-      <h1 className={title}>
-        Mes projets
-        <button className={button} type="button">
-          Nouveau projet
-        </button>
-      </h1>
-      {projects &&
-        (projects.length > 0 ? (
-          projects.map(project => (
-            <Project
-              key={project.id}
-              project={project}
-              onSelect={onSelect(project.id)}
-              selected={selectedProjects[project.id]}
-            />
-          ))
-        ) : (
-          <EmptyProject />
-        ))}
-    </Box>
+    <Steps step={step}>
+      <Box>
+        <h1 className={title}>
+          Mes projets
+          <button className={button} type="button" onClick={() => setStep(1)}>
+            Nouveau projet
+          </button>
+        </h1>
+        {projects &&
+          (projects.length > 0 ? (
+            projects.map(project => (
+              <Project
+                key={project.id}
+                project={project}
+                onSelect={onSelect(project.id)}
+                selected={selectedProjects[project.id]}
+              />
+            ))
+          ) : (
+            <EmptyProject />
+          ))}
+      </Box>
+    </Steps>
   );
 };
 
