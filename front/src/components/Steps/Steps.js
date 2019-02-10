4395b119eb33e0b1239bc6ebed72541c053051ef
Ayc0 <ayc0.benj@gmail.com>
2019-02-10 19:07:27 +0100

    [front] avoid focus in next step

--- front/src/components/Steps/Steps.js
+++ front/src/components/Steps/Steps.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import injectClassName from '/utils/injectClassName';
 
 import { wrapper, hidden_left, visible_middle, hidden_right } from './Steps.css';
 
 const Steps = ({ step = 0, children: c, ...props }) => {
+  const middleRef = useRef(null);
+
+  const focus = () => {
+    const el = middleRef.current;
+    if (!el) {
+      return;
+    }
+    const focEl = el.querySelector('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])');
+    if (!focEl) {
+      return;
+    }
+    focEl.focus();
+  };
+
   const children = React.Children.toArray(c);
   const nbChildren = children.length;
   if (nbChildren === 0) {
@@ -25,11 +39,23 @@ const Steps = ({ step = 0, children: c, ...props }) => {
   } else {
     index = step;
   }
+
   return (
     <div {...props} className={wrapper}>
-      {index - 1 in children && injectClassName(children[index - 1], hidden_left, { key: index - 1 })}
-      {injectClassName(children[index], visible_middle, { key: index })}
-      {index + 1 in children && injectClassName(children[index + 1], hidden_right, { key: index + 1 })}
+      {index - 1 in children &&
+        injectClassName(children[index - 1], hidden_left, {
+          key: index - 1,
+          onTransitionEnd: focus,
+        })}
+      {injectClassName(children[index], visible_middle, {
+        key: index,
+        ref: middleRef,
+      })}
+      {index + 1 in children &&
+        injectClassName(children[index + 1], hidden_right, {
+          key: index + 1,
+          onTransitionEnd: focus,
+        })}
     </div>
   );
 };
