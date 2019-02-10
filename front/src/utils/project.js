9f4abe4c510c080096a1ca5e5ca8e11a08a860d8
Ayc0 <ayc0.benj@gmail.com>
2019-02-10 10:41:44 +0100

    [front] handle new project

---
+++ front/src/utils/project.js
@@ -0,0 +1,30 @@
+import { createCup } from 'manatea';
+
+export const newProjectCup = createCup({
+  name: null,
+  repository_url: null,
+  repository_type: 'github',
+  template_id: null,
+  repository_token: null,
+  external_domain_names: [],
+  use_https: true,
+  docker_image_version: null,
+  root_folder: null,
+  exposed_port: null,
+  system_dependencies: [],
+  dependency_files: [],
+  install_script: null,
+  build_script: null,
+  start_script: null,
+  static_folder: null,
+  databases: [],
+  env: {},
+});
+
+export const isProjectFilled = () => {
+  const project = newProjectCup();
+  if (!project.name || !project.repository_url || !project.template_id || !project.repository_type) {
+    return false;
+  }
+  return true;
+};
