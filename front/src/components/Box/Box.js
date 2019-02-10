7bf2a18e4a9208e2a8ac239dad5907613a7ed569
Ayc0 <ayc0.benj@gmail.com>
2019-02-10 19:07:57 +0100

    [front] forward ref in Box

--- front/src/components/Box/Box.js
+++ front/src/components/Box/Box.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 
 import { box } from './Box.css';
 
-const Box = ({ className: cn, as = 'div', ...props }) => {
+const Box = ({ className: cn, as = 'div', ...props }, ref) => {
   const className = [box];
   if (cn) {
     className.push(cn);
   }
   return React.createElement(as, {
     ...props,
+    ref,
     className: className.join(' '),
   });
 };
 
-export default Box;
+export default forwardRef(Box);
