7b2b950585d19f97b7ba4e1a81543d3159496626
Ayc0 <ayc0.benj@gmail.com>
2019-02-11 05:56:12 +0100

    [front] fetch all repos

--- front/src/containers/Repositories/Repositories.js
+++ front/src/containers/Repositories/Repositories.js
@@ -1,42 +1,38 @@
 import React, { Fragment, useState, useEffect } from 'react';
 
-import { getFullRepos } from '/utils/githubAPI';
+import { getRepos } from '/utils/githubAPI';
 
 import { input, label } from './Repository.css';
 
-const Repository = ({ id, repo, onSelect }) => (
+const Repository = ({ repo, onSelect }) => (
   <Fragment>
     <input
       type="radio"
-      id={id}
+      id={repo.id}
       className={input}
       name="repository_url"
       value={repo.url}
       onChange={() => onSelect(repo)}
     />
-    <label htmlFor={id} className={label}>
-      {repo.name}
+    <label htmlFor={repo.id} className={label}>
+      {repo.full_name}
     </label>
   </Fragment>
 );
 
 const Repositories = ({ onSelect }) => {
-  const [repositories, setRepos] = useState({});
+  const [repositories, setRepos] = useState([]);
+
+  const fetchRepos = () =>
+    getRepos()
+      .then(({ data, headers }) => (console.dir(headers), setRepos([...repositories, ...data])))
+      .catch(console.warn);
 
   useEffect(() => {
-    getFullRepos()
-      .then(({ data }) => setRepos(data))
-      .catch(console.warn);
+    fetchRepos();
   }, []);
 
-  return Object.entries(repositories).map(([org, repos]) => (
-    <Fragment key={org}>
-      <h2>{org}</h2>
-      {repos.map(repo => (
-        <Repository key={`${org}_${repo.id}`} id={`${org}_${repo.id}`} repo={repo} onSelect={onSelect} />
-      ))}
-    </Fragment>
-  ));
+  return repositories.map(repo => <Repository key={repo.id} repo={repo} onSelect={onSelect} />);
 };
 
 export default Repositories;
