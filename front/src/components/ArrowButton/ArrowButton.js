bcf2e2645e23f50d8084e12b4b4e090b00ea4c2b
Ayc0 <ayc0.benj@gmail.com>
2019-02-16 15:44:05 +0100

    [front] change left button

--- front/src/components/ArrowButton/ArrowButton.js
+++ front/src/components/ArrowButton/ArrowButton.js
@@ -2,12 +2,11 @@ import React from 'react';
 
 import { button, next, previous } from './ArrowButton.css';
 
+import cx from '/utils/className';
+
 const ArrowButton = ({ direction = 'next', className: cn, ...props }) => {
-  const className = [button, direction === 'previous' ? previous : next];
-  if (cn) {
-    className.push(cn);
-  }
-  return <button type="button" {...props} className={className.join(' ')} />;
+  const className = [button, direction === 'previous' ? previous : next, cn];
+  return <button type="button" {...props} className={cx(className)} />;
 };
 
 export default ArrowButton;
