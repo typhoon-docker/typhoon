1e6f6478725e7f68d9e33d116954e28ea7d0f41a
Ayc0 <ayc0.benj@gmail.com>
2019-02-20 19:26:22 +0100

    [front] ignore questions

--- front/src/containers/Variables/Variables.js
+++ front/src/containers/Variables/Variables.js
@@ -1,8 +1,86 @@
 import React from 'react';
 
 import { Input, Checkbox } from '/components/Input';
+import globalIgnoreField from '/utils/ignore_fields.json';
 
 const Variables = ({ project }) => {
+  if (!project) {
+    return null;
+  }
+  const {
+    template_id,
+    external_domain_names,
+    root_folder,
+    system_dependencies,
+    dependency_files,
+    install_script,
+    build_script,
+    start_script,
+    static_folder,
+    exposed_port,
+    use_https,
+  } = project;
+
+  const ignoreField = template_id ? globalIgnoreField[template_id] : {};
+
+  const questions = [
+    {
+      title: 'Noms de domaines externes',
+      name: 'external_domain_names',
+      placeholder: 'Exemple : mon-site.fr,www.mon-site.fr (séparés par une virgule, sans https ni / à la fin)',
+      agreement: 'plural',
+      defaultValue: external_domain_names ? external_domain_names.join(',') : '',
+    },
+    {
+      title: 'Dossier contenant le code (monorepo)',
+      name: 'root_folder',
+      placeholder: 'Exemple back',
+      defaultValue: root_folder || '',
+    },
+    {
+      title: 'Dépendances systèmes',
+      name: 'system_dependencies',
+      placeholder: 'Exemple : ffmpeg,imagemagick (séparés par une virgule)',
+      agreement: 'plural',
+      defaultValue: system_dependencies ? system_dependencies.join(',') : '',
+    },
+    {
+      title: 'Fichiers de dépendances',
+      name: 'dependency_files',
+      placeholder: 'Exemple : package.json,yarn.lock (séparés par une virgule)',
+      agreement: 'plural',
+      defaultValue: dependency_files ? dependency_files.join(',') : '',
+    },
+    {
+      title: "Script d'installation",
+      name: 'install_script',
+      defaultValue: install_script || '',
+    },
+    {
+      title: 'Script de build',
+      name: 'build_script',
+      defaultValue: build_script || '',
+    },
+    {
+      title: 'Script de run',
+      name: 'start_script',
+      defaultValue: start_script || '',
+    },
+    {
+      title: 'Dossier statique',
+      placeholder: 'Exemple images',
+      name: 'static_folder',
+      defaultValue: static_folder || '',
+    },
+    {
+      type: 'number',
+      title: "Port d'écoute",
+      name: 'exposed_port',
+      defaultValue: exposed_port || '',
+      min: 80,
+      max: 65535,
+    },
+  ];
   return (
     <>
       <h2 style={{ fontWeight: 600, padding: '1em', fontSize: '1.2em' }}>
@@ -12,72 +90,11 @@ const Variables = ({ project }) => {
         </span>
       </h2>
 
-      <Input
-        title="Noms de domaines externes"
-        name="external_domain_names"
-        placeholder="Exemple : mon-site.fr,www.mon-site.fr (séparés par une virgule, sans https ni / à la fin)"
-        askIfEmpty
-        agreement="plural"
-        defaultValue={project ? project.external_domain_names.join(',') : ''}
-      />
+      {questions.map(question =>
+        ignoreField[question.name] ? null : <Input key={question.name} askIfEmpty {...question} />,
+      )}
 
-      <Input
-        title="Dossier contenant le code (monorepo)"
-        name="root_folder"
-        placeholder="Exemple back"
-        askIfEmpty
-        defaultValue={project ? project.root_folder : ''}
-      />
-
-      <Input
-        title="Dépendances systèmes"
-        name="system_dependencies"
-        placeholder="Exemple : ffmpeg,imagemagick (séparés par une virgule)"
-        askIfEmpty
-        agreement="plural"
-        defaultValue={project ? project.system_dependencies.join(',') : ''}
-      />
-      <Input
-        title="Fichiers de dépendances"
-        name="dependency_files"
-        placeholder="Exemple : package.json,yarn.lock (séparés par une virgule)"
-        askIfEmpty
-        agreement="plural"
-        defaultValue={project ? project.dependency_files.join(',') : ''}
-      />
-      <Input
-        title="Script d'installation"
-        name="install_script"
-        askIfEmpty
-        defaultValue={project ? project.install_script : ''}
-      />
-
-      <Input
-        title="Script de build"
-        name="build_script"
-        askIfEmpty
-        defaultValue={project ? project.build_script : ''}
-      />
-      <Input title="Script de run" name="start_script" askIfEmpty defaultValue={project ? project.start_script : ''} />
-
-      <Input
-        title="Dossier statique"
-        placeholder="Exemple images"
-        name="static_folder"
-        askIfEmpty
-        defaultValue={project ? project.static_folder : ''}
-      />
-
-      <Input
-        type="number"
-        title="Port d'écoute"
-        name="exposed_port"
-        askIfEmpty
-        defaultValue={project ? project.exposed_port : ''}
-        min={80}
-        max={65535}
-      />
-      <Checkbox title="HTTPS" name="use_https" defaultChecked={project ? project.use_https : false} value="https" />
+      <Checkbox title="HTTPS" name="use_https" defaultChecked={project ? use_https : false} value="https" />
     </>
   );
 };
