1463db475c335a3553436ee1bbf18bdc5046d1a1
aymeric <aymeric@feedly.com>
2019-03-10 12:08:06 +0100

    [Front] PR comments addressed

--- front/src/components/LinesBlocks/LinesBlocks.js
+++ front/src/components/LinesBlocks/LinesBlocks.js
@@ -3,35 +3,38 @@ import React from 'react';
 import { getBuildLogs, getDockerFiles } from '/utils/typhoonAPI';
 import useAxios from '/utils/useAxios';
 
-import { block, lines, line } from './LinesBlocks.css';
+import { lbh2, lbh3, block, lines, line } from './LinesBlocks.css';
+
+const OneLinesBlock = (keyName, linesText) => {
+  if (!linesText || keyName.startsWith('_')) {
+    return null;
+  }
+  return (
+    <div key={keyName}>
+      <h3 className={lbh3}>{keyName}</h3>
+      <div className={block}>
+        <pre className={lines}>
+          {linesText
+            .trim()
+            .split('\n')
+            .map((lineText, i) => (
+              // The key is the array index because it will not be refreshed, no need for tracking
+              // eslint-disable-next-line react/no-array-index-key
+              <span className={line} key={i}>
+                {lineText}
+              </span>
+            ))}
+        </pre>
+      </div>
+    </div>
+  );
+};
 
 const LinesBlocks = (linesData, title) => {
   return (
     <>
-      <h2>{title}</h2>
-      {Object.keys(linesData).map(keyName => {
-        if (!linesData[keyName] || keyName.startsWith('_')) {
-          return null;
-        }
-        return (
-          <div key={keyName}>
-            <h3>{keyName}</h3>
-            <div className={block}>
-              <pre className={lines}>
-                {linesData[keyName]
-                  .trim()
-                  .split('\n')
-                  .map((lineText, i) => (
-                    // eslint-disable-next-line react/no-array-index-key
-                    <span className={line} key={i}>
-                      {lineText}
-                    </span>
-                  ))}
-              </pre>
-            </div>
-          </div>
-        );
-      })}
+      <h2 className={lbh2}>{title}</h2>
+      {Object.keys(linesData).map(keyName => OneLinesBlock(keyName, linesData[keyName]))}
     </>
   );
 };
